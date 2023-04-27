cad31aa5619ef754101dbec869a4c218d2ee5053
HushchinVolodymyr <114146250+HushchinVolodymyr@users.noreply.github.com>
2023-04-27 23:09:16 +0300

    adaptive landing

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,12 @@
 var header_logo = document.getElementById("header__logo");
-var header_username = document.getElementById("username");
 
 function change_logo_resize(){
     var width = window.innerWidth;
     
     if(width > 620){
         header_logo.innerHTML="OnlineGallery"
-        //header_username.innerHTML="Username"
     } else if(width <= 620){
         header_logo.innerHTML="OG"
-        //header_username.innerHTML=""
     }
 }
 
